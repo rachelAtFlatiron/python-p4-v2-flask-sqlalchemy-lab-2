f3634962aceed3822e49e11eb50fff8228eddfc9
Rachel Katz <rachel.katz@flatironschool.com>
2024-08-29 15:02:30 -0400

    demo done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Review, Item, Customer
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -16,6 +16,20 @@ db.init_app(app)
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
 
+@app.route('/reviews')
+def all_reviews():
+    q = Review.query.all()
+    return jsonify([r.to_dict() for r in q]), 200
+
+@app.route('/items')
+def all_items():
+    q = Item.query.all()
+    return jsonify([i.to_dict(rules=('-reviews.item_id', '-reviews.customer_id')) for i in q]), 200
+
+@app.route('/customers')
+def all_customers():
+    q = Customer.query.all()
+    return jsonify([c.to_dict() for c in q]), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,22 +11,80 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    #customer_reviews
+    customer_reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('customer_reviews', 'item')
+
+    # to test serialize_rules...POSTMAN (you'll have to write routes)
+    # or in flask shell -> customer_instance.to_dict()
+    serialize_rules = ('-customer_reviews.customer', )
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
+    
+    """
+    customer: {
+        id,
+        name,
+        customer_reviews: [{
+            id, 
+            comment,
+            customer: {
+                id,
+                name,
+                customer_reviews: [{
+                    id,
+                    comment,
+                    customer...
+                }]
+            },
+            item: {}
+        }, ...],
+        items: [{
+            id,
+            name,
+            price,
+            reviews: []
+        }, ...]
+    }
+    """
+    
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    #item_reviews
+    item_reviews = db.relationship('Review', back_populates='item')
+
+    serialize_rules = ('-item_reviews.item', )
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+    
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    #refers to customer_reviews
+    customer = db.relationship('Customer', back_populates='customer_reviews')
+    #refers to item_reviews
+    item = db.relationship('Item', back_populates='item_reviews')
+
+
+
+    serialize_rules = ('-customer.customer_reviews', '-item.item_reviews')
